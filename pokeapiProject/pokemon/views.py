abe73b0427502ab9e797b321e098f21e01cc48f4
10-15-5 <hpjpm@protonmail.com>
2022-10-22 17:38:30 +0100

    added templates and new pages

--- pokeapiProject/pokemon/views.py
+++ pokeapiProject/pokemon/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. Welcome to my Pokemon API project")
+
+
+def home(request):
+    context = {"name":"user"}
+    return render(request, 'test.html', context)
+
+
+def about(request):
+    return render(request, 'test-about.html')
