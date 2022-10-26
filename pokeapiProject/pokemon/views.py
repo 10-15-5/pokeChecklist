41f3d9e7051339bf2eeee1d3da253ad3413aae12
10-15-5 <hpjpm@protonmail.com>
2022-10-26 21:02:42 +0100

    List of gen 1 Pokemon added

--- pokeapiProject/pokemon/views.py
+++ pokeapiProject/pokemon/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 
 def index(request):
-    return HttpResponse("Hello, world. Welcome to my Pokemon API project")
+
+    x = requests.get('https://pokeapi.co/api/v2/pokedex/kanto').json()
+
+    context = {"name": x["pokemon_entries"]}
+
+    return render(request, 'test.html', context)
 
 
 def home(request):
